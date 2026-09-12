57528d6fe913920a56033688d41e38d66702e00f
geomlab <edmondsandkarp@gmail.com>
2026-09-12 23:25:07 +0200

    Promote updater recovery sandbox fix

--- stable-bootstrap.sh
+++ stable-bootstrap.sh
@@ -4,7 +4,7 @@ set +x
 umask 077
 
 readonly PUBLIC_STAGE0_SHA='acdc19676200931cc9b61cfaa5292b4a0ccab846'
-readonly INFRA_SHA='cd5d6c13ae0a4fbacbfbc8433efd4c309c2f8a2a'
+readonly INFRA_SHA='979a1282d6211b2b78de1524d240e91095d7bcb5'
 readonly PUBLIC_BOOTSTRAP_URL="https://raw.githubusercontent.com/geomlab/gwcut-public/${PUBLIC_STAGE0_SHA}/bootstrap.sh"
 
 fatal() {
