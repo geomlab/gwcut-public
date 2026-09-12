11e579cab1ad86c30459dca37ff76f7b1723dad0
geomlab <edmondsandkarp@gmail.com>
2026-09-12 22:44:49 +0200

    Promote backup freshness marker fix

--- stable-bootstrap.sh
+++ stable-bootstrap.sh
@@ -4,7 +4,7 @@ set +x
 umask 077
 
 readonly PUBLIC_STAGE0_SHA='acdc19676200931cc9b61cfaa5292b4a0ccab846'
-readonly INFRA_SHA='7e4a1d8c3ef53320f32d30ce8e1e1934399a2c02'
+readonly INFRA_SHA='f3a8b4c0eb1d57d1460efa710ed923542e626e2d'
 readonly PUBLIC_BOOTSTRAP_URL="https://raw.githubusercontent.com/geomlab/gwcut-public/${PUBLIC_STAGE0_SHA}/bootstrap.sh"
 
 fatal() {
